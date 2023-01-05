31d9cb7915483392f006b2327972b0585031ed87
Y4NK35 <jakubpanek1010@gmail.com>
2023-01-05 20:33:47 +0100

    Initial commit: BubbleSortProgram

---
+++ src/BubbleSortAlgorithm.java
@@ -0,0 +1,47 @@
+public class BubbleSortAlgorithm {
+    private int []result ;
+
+    public void sortArray(int[] inputArray){
+        if(isArrayEmpty(inputArray)){
+            System.out.println("Array is empty.");
+            result =inputArray;
+        }else{
+            int arrayLength =inputArray.length;
+            for(int i = 0; i<arrayLength; i++){
+                for(int j=0; j<arrayLength-1;j++){
+                    if(inputArray[j]>inputArray[j+1]) {
+                        int tempInt = inputArray[j+1];
+                        inputArray[j+1]= inputArray[j];
+                        inputArray[j]= tempInt;
+                    }
+                }
+            }
+            result = inputArray;
+        }
+
+
+    }
+
+    public boolean isArrayEmpty (int[] array){
+        return array.length==0;
+    }
+    public String printArray(int[] array){
+        if(array.length==0){
+            return "{}";
+        }else {
+            StringBuilder sb = new StringBuilder();
+            sb.append("{");
+            for (int i = 0; i < array.length; i++) {
+                sb.append(array[i] + ", ");
+            }
+            sb.deleteCharAt(sb.length() - 1);
+            sb.deleteCharAt(sb.length() - 1);
+            sb.append('}');
+            return sb.toString();
+        }
+    }
+
+    public int[] getResult() {
+        return result;
+    }
+}
---
+++ src/BubbleSortProgram.java
@@ -0,0 +1,10 @@
+
+
+public class BubbleSortProgram {
+    public static void main(String[] args) throws InterruptedException {
+        TestClass test = new TestClass();
+        test.startTests();
+
+
+    }
+}
